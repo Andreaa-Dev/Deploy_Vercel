84a04c5d71957643c7b6f4f7d257f5fb16190278
Andrea <ngocanhhsgs@gmail.com>
2023-01-02 15:03:21 +0200

    add change in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">deploy</div>;
+  return <div className="App">Deploy by Vercel</div>;
 }
 
 export default App;
